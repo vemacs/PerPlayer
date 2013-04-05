9792cfcf10db8e97688a165f2f7e2d39d2fc6a1b
vemacs <gentoo@outlook.com>
2013-04-04 18:04:11 -0600

    Initial commit

---
+++ src/main/java/com/nullblock/vemacs/perplayer/PerPlayer.java
@@ -0,0 +1,27 @@
+package com.nullblock.vemacs.perplayer;
+
+import java.util.logging.Logger;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.nullblock.vemacs.perplayer.threads.MonitorThread;
+
+public class PerPlayer extends JavaPlugin implements Listener {
+		
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+    }
+
+    public void onEnable() {
+    	//config handler
+    	this.saveDefaultConfig();
+    	int limit = this.getConfig().getInt("limit");
+    	int safe = this.getConfig().getInt("safe");
+    	int radius = this.getConfig().getInt("radius");
+    	//start thread
+        new Thread(new MonitorThread(limit, safe, radius)).start();
+    }
+}
